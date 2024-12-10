495fd412ea1ce9948f8921bbe1d61a7ef1f9481b
LPopivnyak <141331552+olenapolianska@users.noreply.github.com>
2024-12-10 17:27:56 +0200

    dsadas

--- game1.py
+++ game1.py
@@ -1,14 +1,11 @@
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
 
-
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.model = loader.loadModel("models/environment")
-        self.model.reparentTo(render)
-        self.model.setScale(0.1)
-        self.model.setPos(-2, 25, -3)
+        self.land = Mapmanager()
+        base.camLens.setFor(90)
 
 game = Game()
 game.run()
